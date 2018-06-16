23a1ec8e057449ed3ef43563cf25cb7469666e87
Rohan Chougule <rohan183chougule@gmail.com>
2018-06-17 02:50:15 +0530

    components creation

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningComponent,
+    SuccessComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
